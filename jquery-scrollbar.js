bf5b733605d66809aa330a0bd2e290f296fea8cc
Abdul Baquee <abdulbaquee@gmail.com>
2015-06-03 17:31:12 +0530

    Jquery Page Scrollbar indicator features added

---
+++ jquery-scrollbar.js
@@ -0,0 +1,25 @@
+(function ($) {
+    $.fn.onscroll = function (options) {
+        // This is the easiest way to have default options.
+        var settings = $.extend({
+            backgroundColor: "#f00",
+            height: '10px',
+            position: 'fixed'
+        }, options);
+        var mySelector = this.selector;
+        this.each(function () {
+            $(window).scroll(function () {
+                var offsettop = parseInt($(this).scrollTop());
+                var parentHeight = parseInt($('body, html').height() - $(window).height());
+                var vscrollwidth = offsettop / parentHeight * 100;
+                $(mySelector).css({width: vscrollwidth + '%'});
+            });
+            $(mySelector).css({
+                backgroundColor: settings.backgroundColor,
+                height: settings.height,
+                position: settings.position
+            });
+        });
+        return this;
+    };
+}(jQuery));
